30bd965a1ee1a10fa8beba03e0276d6745c6bd01
MrJackey <44698252+MrJackey@users.noreply.github.com>
2021-01-28 16:42:58 +0100

    Make menu prototype

---
+++ Assets/PlayerEventHandling.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerEventHandling : MonoBehaviour {
+	public Health health;
+	public SceneReference MenuScene;
+
+	private void Start() {
+		health.onDeath.AddListener(HandleDeath);
+	}
+
+	private void HandleDeath() {
+		SceneManager.LoadScene(MenuScene);
+	}
+}
---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+	public SceneReference GameScene;
+
+
+	public void StartGame() {
+		SceneManager.LoadScene(GameScene);
+	}
+
+	public void QuitGame() {
+		#if UNITY_EDITOR
+				UnityEditor.EditorApplication.ExitPlaymode();
+		#else
+				Application.Quit();
+		#endif
+	}
+}
