72e27e86f4b1f0578ca860fd3a04c64310d0fbb0
MrJackey <44698252+MrJackey@users.noreply.github.com>
2021-01-29 19:28:31 +0100

    Give Rat walking animations

--- Assets/Scripts/Enemies/Rat.cs
+++ Assets/Scripts/Enemies/Rat.cs
@@ -3,18 +3,37 @@ using DG.Tweening;
 using UnityEngine;
 
 public class Rat : MonoBehaviour {
+	private static readonly int speedYHash = Animator.StringToHash("speedY");
+	private static readonly int speedXHash = Animator.StringToHash("speedX");
+
 	public Transform backpack;
 	public GameObject weapon;
 	public Collider2D navigationCollider;
 	public float deathFadeDuration = 1f;
 
 	private Toy toy;
+	private Animator animator;
+	private Rigidbody2D rb2d;
 
 	private void Start() {
 		toy = ToyManager.Instance.CreateRandom();
 		toy.transform.SetParent(backpack, false);
 
 		GetComponent<Health>().onDeath.AddListener(OnDeath);
+		animator = GetComponent<Animator>();
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update() {
+		Vector3 localScale = transform.localScale;
+		if (rb2d.velocity.x > 0)
+			localScale.x = -1;
+		else if (rb2d.velocity.x < 0)
+			localScale.x = 1;
+		transform.localScale = localScale;
+
+		animator.SetFloat(speedYHash, rb2d.velocity.y);
+		animator.SetFloat(speedXHash, rb2d.velocity.x);
 	}
 
 	private void OnDeath() {
