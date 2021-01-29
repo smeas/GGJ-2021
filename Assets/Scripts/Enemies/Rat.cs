6d0540d489e8c75a190de873d25049a399374d8a
Jonatan Johansson <smeas@users.noreply.github.com>
2021-01-29 19:13:38 +0100

    Fix rats becoming ghosts when they die

--- Assets/Scripts/Enemies/Rat.cs
+++ Assets/Scripts/Enemies/Rat.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Rat : MonoBehaviour {
 	public Transform backpack;
+	public GameObject weapon;
+	public Collider2D navigationCollider;
 	public float deathFadeDuration = 1f;
 
 	private Toy toy;
@@ -17,6 +19,12 @@ public class Rat : MonoBehaviour {
 
 	private void OnDeath() {
 		toy.Drop(transform);
+
+		weapon.SetActive(false);
+		navigationCollider.enabled = false;
+		GetComponent<Collider2D>().enabled = false;
+		GetComponent<RatAI>().enabled = false;
+
 		GetComponent<SpriteRenderer>()
 			.DOFade(0, deathFadeDuration)
 			.OnComplete(() => Destroy(gameObject));
