94bf939ba4522f126b3acefc1d7e256cd3f8cc55
MrJackey <44698252+MrJackey@users.noreply.github.com>
2021-01-30 16:11:37 +0100

    Implement pause menu

--- Assets/Scripts/UI/ButtonHoverEffect.cs
+++ Assets/Scripts/UI/ButtonHoverEffect.cs
@@ -2,31 +2,36 @@ using DG.Tweening;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler {
+public class ButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler,
+	IPointerDownHandler, IPointerUpHandler {
 	public float scaleAmount = 1.5f;
 	public float duration = 0.5f;
 
-	private bool isHovering = false;
+	private bool isHovering;
+
+	public void OnPointerDown(PointerEventData eventData) {
+		transform.DOKill();
+		transform.DOScale(1f, duration / 2f)
+			.SetUpdate(UpdateType.Normal, true);
+	}
 
 	public void OnPointerEnter(PointerEventData eventData) {
 		isHovering = true;
 		transform.DOKill();
-		transform.DOScale(scaleAmount, duration);
+		transform.DOScale(scaleAmount, duration)
+			.SetUpdate(UpdateType.Normal, true);
 	}
 
 	public void OnPointerExit(PointerEventData eventData) {
 		isHovering = false;
 		transform.DOKill();
-		transform.DOScale(1f, duration);
-	}
-
-	public void OnPointerDown(PointerEventData eventData) {
-		transform.DOKill();
-		transform.DOScale(1f, duration / 2f);
+		transform.DOScale(1f, duration)
+			.SetUpdate(UpdateType.Normal, true);
 	}
 
 	public void OnPointerUp(PointerEventData eventData) {
 		transform.DOKill();
-		transform.DOScale(isHovering ? scaleAmount : 1f, duration / 2f);
+		transform.DOScale(isHovering ? scaleAmount : 1f, duration / 2f)
+			.SetUpdate(UpdateType.Normal, true);
 	}
 }
---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+	public GameObject pauseMenu;
+	public SceneReference mainmenuScene;
+
+	private bool isPaused;
+
+	private void Start() {
+		pauseMenu.SetActive(false);
+	}
+
+	private void Update() {
+		if (Input.GetButtonDown("Cancel")) {
+			if (isPaused)
+				Unpause();
+			else
+				Pause();
+		}
+	}
+
+	public void Pause() {
+		if (isPaused) return;
+
+		Time.timeScale = 0;
+		isPaused = true;
+		pauseMenu.SetActive(true);
+	}
+
+	public void Unpause() {
+		if (!isPaused) return;
+
+		Time.timeScale = 1;
+		isPaused = false;
+		pauseMenu.SetActive(false);
+	}
+
+	public void ReturnToMenu() {
+		Time.timeScale = 1;
+		SceneManager.LoadScene(mainmenuScene);
+	}
+}
