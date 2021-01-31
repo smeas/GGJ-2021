5019ec29f21a7d9854979e681a30692efd8eaa1f
MrJackey <44698252+MrJackey@users.noreply.github.com>
2021-01-31 15:21:59 +0100

    Add intro cutscene on game start

---
+++ Assets/Scripts/UI/IntroHelper.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroHelper : MonoBehaviour {
+	public SceneReference menuScene;
+
+	private void HandleIntroEnd() {
+		SceneManager.LoadScene(menuScene);
+	}
+}
