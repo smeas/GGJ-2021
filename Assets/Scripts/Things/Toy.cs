f0e01af6f1c33284e555eae7bcfd2915c6676484
Jonatan Johansson <smeas@users.noreply.github.com>
2021-01-31 10:37:00 +0100

    Make toy bounce on dropped

--- Assets/Scripts/Things/Toy.cs
+++ Assets/Scripts/Things/Toy.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using DG.Tweening;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Toy : MonoBehaviour {
 	public ToyData data;
+	public float maxDropDisplacement = 0.8f;
+	public float dropBounceHeight = 1f;
+	public float dropBounceDuration = 0.5f;
 
 	private Collider2D pickupTrigger;
 
@@ -14,6 +19,7 @@ public class Toy : MonoBehaviour {
 	private void OnTriggerEnter2D(Collider2D other) {
 		if (!other.isTrigger && other.attachedRigidbody.CompareTag("Player")) {
 			GameManager.Instance.AddScore(data);
+			transform.DOKill();
 			Destroy(gameObject);
 		}
 	}
@@ -21,5 +27,14 @@ public class Toy : MonoBehaviour {
 	public void Drop(Transform root) {
 		transform.parent = root.parent;
 		pickupTrigger.enabled = true;
+
+		float dx = Random.Range(-maxDropDisplacement, maxDropDisplacement);
+		Vector3 myPosition = transform.position;
+		DOTween.Sequence()
+			.Append(transform.DOMoveY(myPosition.y + dropBounceHeight, dropBounceDuration * 0.5f).SetEase(Ease.OutQuad))
+			.Append(transform.DOMoveY(myPosition.y, dropBounceDuration * 0.5f).SetEase(Ease.InQuad))
+			.Insert(0, transform.DOMoveX(myPosition.x + dx, dropBounceDuration).SetEase(Ease.InQuad))
+			.SetLink(gameObject)
+			.Play();
 	}
 }
